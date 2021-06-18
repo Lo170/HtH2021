08b5a44bd8ac20de7c04c4da96073d355a63a075
Lola Aruda <Lola@hackthehood.org>
2021-06-17 18:47:47 -0700

    practice files

---
+++ dictionary_practice.py
@@ -0,0 +1,4 @@
+groceries = {"chicken": "1.56" , "beef": "1.00" , "cheese": "1.25"}
+chicken_price = groceries ["chicken"]
+print(chicken_price)
+print("hello world")
---
+++ list_practice.py
@@ -0,0 +1,19 @@
+empty_list = []
+
+cities = ["Oakland", "Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver"]
+print (cities)
+print (cities[0])
+print (cities[2])
+print (cities[5])
+
+cities [0] = "San Fransico"
+cities[2] = "Brooklyn"
+cities[7] = "Hollywood"
+cities [5] = "Tampa"
+print (cities) 
+
+names =["Lola", "Georgia", "Nicole", "Eric", "Justin", "Kyla","Tiare", "Devon","Steven", "Cathy"]
+print (names)
+Two_names = names[0:2]
+print (Two_names)
+
